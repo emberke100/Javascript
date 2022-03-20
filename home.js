22cfb4894d4945ca54d6c6b23447c256b38d1a97
Szabó Dániel <emberke100ful@gmail.com>
2022-03-20 21:55:44 +0100

    Testing jquery

--- home.js
+++ home.js
@@ -1,12 +1,17 @@
-//document.getElementById('sText').innerHTML = "Hey Friend"
-
-//var age = prompt('What is your age?');
-
-var q = 10;
-window.setInterval(myFunction, 100);
-
 function myFunction() {
-    document.getElementById('sText').innerHTML = q;
-    q++;
+    document.getElementById("myDropdown").classList.toggle("show");
   }
   
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
